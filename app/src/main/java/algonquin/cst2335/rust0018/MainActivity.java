7a1c7cf0b14d60a0cc471fa8c86b6ff00d03aae4
Said Rustom <said-rustom@outlook.com>
2023-01-11 19:25:00 -0500

    Initial Commit

---
+++ app/src/main/java/algonquin/cst2335/rust0018/MainActivity.java
@@ -0,0 +1,19 @@
+package algonquin.cst2335.rust0018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override // this starts the application
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // loads buttons, text on screen
+        setContentView(R.layout.activity_main);
+            //R means res
+            //layout is the folder
+            //activity_main is the file
+    }
+}

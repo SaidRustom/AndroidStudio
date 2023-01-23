cb632550f9cac95a37899265d54119723e445339
Said Rustom <said-rustom@outlook.com>
2023-01-23 17:05:10 -0500

    Week2 - Used binding to manipulate layout content.

--- app/src/main/java/algonquin/cst2335/rust0018/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rust0018/MainActivity.java
@@ -4,18 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import algonquin.cst2335.rust0018.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override // this starts the application
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        binding = ActivityMainBinding.inflate( getLayoutInflater() ); //produces layout
+
+
         // loads buttons, text on screen
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot()); // load stuff on screen
+
             /*
             R means res
             layout is the folder
             activity_main is the file
             */
+
+        binding.button.setOnClickListener( v -> {
+            String editString = binding.editText.getText().toString();
+            binding.textView.setText(editString);
+            binding.editText.setText(editString);
+            binding.button.setText(editString);
+        });
     }
 }
